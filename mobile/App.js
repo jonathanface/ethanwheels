3b20ed9d33b556105d203560f63cbd1025af49b4
Jonathan Face <=>
2020-02-21 12:51:47 -0500

    basic speed slider

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import BigSlider from 'react-native-big-slider'
+
+export default class bigSlider extends Component {
+  constructor() {
+    super()
+    this.state = { speed:50 }
+  }
+  render () {
+    return (
+      <View style={styles.root}>
+        <View style={styles.container}>
+          <Text style={{marginRight:10}}>Speed</Text>
+          <BigSlider style={styles.slider} minimumValue={0} maximumValue={100}
+            label={`${this.state.speed | 0}%`}
+            value={this.state.speed} onValueChange={valA => {
+              this.setState({speed: valA })
+            }} />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  root: {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'white',
+    alignItems:'center'
   },
+  container: {
+    width:400,
+    justifyContent:'center',
+    alignItems:'top',
+    flexDirection:'row',
+    padding:10,
+  },
+  slider: {
+    flexDirection:'column',
+    height:100,
+    width:200,
+  }
 });
+
+AppRegistry.registerComponent('bigSlider', () => bigSlider);
