c090b1eddeee68e45b1e0b6f64f9aabe41745c85
Jonathan Face <=>
2020-02-26 23:54:07 -0500

    on the verge but broken

--- mobile/app/src/main/java/com/jface/ethanwheels/MainActivity.java
+++ mobile/app/src/main/java/com/jface/ethanwheels/MainActivity.java
@@ -4,7 +4,12 @@ import android.Manifest;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
@@ -24,6 +29,10 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import androidx.appcompat.widget.AppCompatSeekBar;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     BluetoothManager btManager;
@@ -36,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
     private final static int REQUEST_ENABLE_BT = 1;
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
-
+    private static final String POWERWHEELS_BLE_ADDRESS = "00:35:FF:1F:74:4E";
     private int speed = 0;
 
     @Override
@@ -106,19 +115,159 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private BluetoothGatt bluetoothGatt;
+    private int connectionState = STATE_DISCONNECTED;
+
+    private static final int STATE_DISCONNECTED = 0;
+    private static final int STATE_CONNECTING = 1;
+    private static final int STATE_CONNECTED = 2;
+
+    private final static String TAG = MainActivity.class.getSimpleName();
+
+    public final static String ACTION_GATT_CONNECTED =
+            "com.example.bluetooth.le.ACTION_GATT_CONNECTED";
+    public final static String ACTION_GATT_DISCONNECTED =
+            "com.example.bluetooth.le.ACTION_GATT_DISCONNECTED";
+    public final static String ACTION_GATT_SERVICES_DISCOVERED =
+            "com.example.bluetooth.le.ACTION_GATT_SERVICES_DISCOVERED";
+    public final static String ACTION_DATA_AVAILABLE =
+            "com.example.bluetooth.le.ACTION_DATA_AVAILABLE";
+    public final static String EXTRA_DATA =
+            "com.example.bluetooth.le.EXTRA_DATA";
+
+    public static String HM_10 = "0000ffe1-0000-1000-8000-00805f9b34fb";
+    private static HashMap<String, String> attributes = new HashMap();
+    static {
+        // Sample Services.
+        attributes.put("0000ffe0-0000-1000-8000-00805f9b34fb", "HM-10 Service");
+        attributes.put(HM_10, "HM-10 Module");
+    }
+    private BluetoothGattCharacteristic characteristicTX;
+    private final BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            String intentAction;
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                intentAction = ACTION_GATT_CONNECTED;
+                connectionState = STATE_CONNECTED;
+                broadcastUpdate(intentAction);
+
+                peripheralTextView.append("Connected to GATT server.");
+                Log.i(TAG, "Attempting to start service discovery:" + bluetoothGatt.discoverServices());
+
+
+
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                intentAction = ACTION_GATT_DISCONNECTED;
+                connectionState = STATE_DISCONNECTED;
+                peripheralTextView.append("Disconnected from GATT server.");
+                broadcastUpdate(intentAction);
+            }
+        }
+
+        @Override
+        // New services discovered
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                broadcastUpdate(ACTION_GATT_SERVICES_DISCOVERED);
+            } else {
+                peripheralTextView.append("onServicesDiscovered received: " + String.valueOf(status));
+            }
+        }
+
+        @Override
+        // Result of a characteristic read operation
+        public void onCharacteristicRead(BluetoothGatt gatt,
+                                         BluetoothGattCharacteristic characteristic,
+                                         int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                peripheralTextView.append("Read characteristic: " + characteristic.toString());
+            }
+        }
+    };
+
+    public void writeCharacteristic(BluetoothGattCharacteristic characteristic) {
+        if (btAdapter == null || bluetoothGatt == null) {
+            Log.w(TAG, "BluetoothAdapter not initialized");
+            return;
+        }
+
+        bluetoothGatt.writeCharacteristic(characteristic);
+    }
+
+    private void broadcastUpdate(final String action) {
+        final Intent intent = new Intent(action);
+        peripheralTextView.append("Discovered " + action + "\n");
+        switch(action) {
+            case ACTION_GATT_SERVICES_DISCOVERED:
+
+                for (BluetoothGattService gattService : gattServices) {
+                    HashMap<String, String> currentServiceData = new HashMap<String, String>();
+                    uuid = gattService.getUuid().toString();
+                    currentServiceData.put(
+                            LIST_NAME, SampleGattAttributes.lookup(uuid, unknownServiceString));
+
+                    // If the service exists for HM 10 Serial, say so.
+                    if(SampleGattAttributes.lookup(uuid, unknownServiceString) == "HM 10 Serial") { isSerial.setText("Yes, serial :-)"); } else {  isSerial.setText("No, serial :-("); }
+                    currentServiceData.put(LIST_UUID, uuid);
+                    gattServiceData.add(currentServiceData);
+
+                    // get characteristic when UUID matches RX/TX UUID
+                    characteristicTX = gattService.getCharacteristic(BluetoothLeService.UUID_HM_RX_TX);
+                    characteristicRX = gattService.getCharacteristic(BluetoothLeService.UUID_HM_RX_TX);
+                }
+                BluetoothGattService gattService
+                characteristicTX = gattService.getCharacteristic(HM_10);
+                break;
+        }
+        sendBroadcast(intent);
+    }
+
+
+
+    private void broadcastUpdate(final String action,
+                                 final BluetoothGattCharacteristic characteristic) {
+        final Intent intent = new Intent(action);
+        Log.v("AndroidLE", "broadcastUpdate()");
+
+        final byte[] data = characteristic.getValue();
+
+        Log.v("AndroidLE", "data.length: " + data.length);
+
+        if (data != null && data.length > 0) {
+            final StringBuilder stringBuilder = new StringBuilder(data.length);
+            for(byte byteChar : data) {
+                stringBuilder.append(String.format("%02X ", byteChar));
+
+                peripheralTextView.append(String.format("%02X ", byteChar));
+            }
+            intent.putExtra(EXTRA_DATA, new String(data) + "\n" + stringBuilder.toString());
+        }
+
+        sendBroadcast(intent);
+    }
+
+    private void connectToPW(BluetoothDevice device) {
+        bluetoothGatt = device.connectGatt(this, false, gattCallback);
+        connectionState = STATE_CONNECTING;
+
+    }
     // Device scan callback.
     private ScanCallback leScanCallback = new ScanCallback() {
         @Override
         public void onScanResult(int callbackType, ScanResult result) {
-            peripheralTextView.append("Device Name: " + result.getDevice().getName() + " rssi: " + result.getRssi() + "\n");
-
-            // auto scroll for text view
+            Log.d("scanning", result.getDevice().getAddress());
+            if (result.getDevice().getAddress().equalsIgnoreCase(POWERWHEELS_BLE_ADDRESS)) {
+                peripheralTextView.append("Found device, stopping scan and connecting.\n");
+                stopScanning();
+                connectToPW(result.getDevice());
+                return;
+            }
             final int scrollAmount = peripheralTextView.getLayout().getLineTop(peripheralTextView.getLineCount()) - peripheralTextView.getHeight();
-            // if there is no need to scroll, scrollAmount will be <=0
-            if (scrollAmount > 0)
+            if (scrollAmount > 0) {
                 peripheralTextView.scrollTo(0, scrollAmount);
+            }
 
-            //do connection here
         }
     };
 
@@ -149,8 +298,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startScanning() {
-        System.out.println("start scanning");
-        peripheralTextView.setText("");
+        peripheralTextView.setText("Scanning...\n");
         startScanningButton.setVisibility(View.INVISIBLE);
         stopScanningButton.setVisibility(View.VISIBLE);
         AsyncTask.execute(new Runnable() {
@@ -162,8 +310,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void stopScanning() {
-        System.out.println("stopping scanning");
-        peripheralTextView.append("Stopped Scanning");
+        peripheralTextView.append("Stopped Scanning.\n");
         startScanningButton.setVisibility(View.VISIBLE);
         stopScanningButton.setVisibility(View.INVISIBLE);
         AsyncTask.execute(new Runnable() {
