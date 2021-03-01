33170adde85d39c0a646f51a369dd91cbd86e596
OSC\jonathanf <jonathan.face@ontariosystems.com>
2021-03-01 00:21:18 -0500

    vendoring motor driver and partial firmware rewrite

--- firmware/POWER_WHEELS/POWER_WHEELS.ino
+++ firmware/POWER_WHEELS/POWER_WHEELS.ino
@@ -1,349 +1,159 @@
+
 #include <Cytron_SmartDriveDuo.h>
-#include <SoftwareSerial.h>
-#include <EEPROM.h>
 
-//Motor pins
-#define BLE_MOTOR_A_INPUT_1 4 
-#define BLE_MOTOR_B_INPUT_1 5 
-#define BLE_MOTOR_B_INPUT_2 6 
-#define BLE_MOTOR_A_INPUT_2 7
+#define GAS_PEDAL_INPUT 11
+#define HEADLIGHTS_SWITCH_INPUT A5
+#define DIRECTION_SWITCH_INPUT 3
+#define MOTOR_A_INPUT_1 4
+#define MOTOR_A_INPUT_2 7
+#define MOTOR_B_INPUT_1 5 
+#define MOTOR_B_INPUT_2 6 
+#define HAZARDS_SWITCH_INPUT 9
 
-#define PILOT_MOTOR_A_INPUT_1 8 
-#define PILOT_MOTOR_B_INPUT_1 9 
-#define PILOT_MOTOR_B_INPUT_2 10 
-#define PILOT_MOTOR_A_INPUT_2 11
-
-#define PILOT_MOTOR_REVERSE_SWITCH 13
-
-//BLE pins
-#define RX 0
-#define TX 1
-
-// LED pins
-#define LED_BUTTON_INPUT A5
-#define HAZARDS_SWITCH  A4
 #define LED_OUTPUTS_SIZE 2
 
 const int LED_OUTPUTS[LED_OUTPUTS_SIZE] = {A0, A1};
 const int HAZARDS_INTERVAL = 500; //ms
 const char* BLE_DELIMITER = "|";
 
-enum communication {
-  COMMAND_REQUEST_STATUS = 100,
-  COMMAND_REQUEST_STATUS_REPLY,
-  COMMAND_DISCONNECT = 102,
-  COMMAND_SPEED_CHANGE = 200,
-  COMMAND_FORWARD = 300,
-  COMMAND_REVERSE,
-  COMMAND_LEFT,
-  COMMAND_RIGHT,
-  COMMAND_STOP,
-  COMMAND_REVERSE_LEFT,
-  COMMAND_REVERSE_RIGHT,
-  COMMAND_LIGHTS_ON = 400,
-  COMMAND_LIGHTS_OFF,
-  COMMAND_HAZARDS_ON,
-  COMMAND_HAZARDS_OFF
-};
+int headlightsState, lastHeadlightsState;
+int reverseState, lastReverseState;
+int hazardsState, lastHazardsState;
+int gasState, lastGasState;
+int previousHazardsMillis = 0;
+bool hazardsSequence = false;
 
-// Globals to keep track of settings
-int lightSwitchState = LOW;
-unsigned long previousMillis = millis();
-bool hazardState = false;
-bool inHazardsMode = false;
-char appData;  
-String inData = "";
-bool bleConnected = false;
+unsigned long debounceDelay = 50;
+unsigned long lastHeadlightsDebounceTime = 0, lastReverseDebounceTime = 0,
+    lastHazardsDebounceTime = 0, lastGasDebounceTime = 0;
+
 int maxMotorSpeed = 25;
-
-int currentHeading = COMMAND_STOP;
-int previousHeading = COMMAND_STOP;
-bool toggleOnLights = false;
-bool toggleOffLights = false;
-bool toggleOnHazards = false;
-bool toggleOffHazards = false;
-
-Cytron_SmartDriveDuo bleMotor(PWM_INDEPENDENT, BLE_MOTOR_A_INPUT_1, BLE_MOTOR_A_INPUT_2, BLE_MOTOR_B_INPUT_1, BLE_MOTOR_B_INPUT_2);
-Cytron_SmartDriveDuo pilotMotor(PWM_INDEPENDENT, PILOT_MOTOR_A_INPUT_1, PILOT_MOTOR_A_INPUT_2, PILOT_MOTOR_B_INPUT_1, PILOT_MOTOR_B_INPUT_2);
-SoftwareSerial HM10(RX, TX);
-
-void processBLECommand(String data) {
-  consolePrint("prcessing " + data);
-  char str_array[data.length()];
-  int parameterIndex = data.indexOf("=");
-  int command;
-  String value;
-  if (parameterIndex > -1) {
-      String parsed = data.substring(0, parameterIndex);
-      parsed.trim();
-      command = parsed.toInt();
-      parsed = data.substring(parameterIndex+1, data.length());
-      parsed.trim();
-      value = parsed;
-  } else {
-    data.trim();
-    command = data.toInt();
-  }
-  consolePrint("Command: " + String(command));
-
-  switch(command) {
-    case COMMAND_REQUEST_STATUS: {
-      String response = String(COMMAND_REQUEST_STATUS_REPLY) + "&speed=" + String(maxMotorSpeed);
-      String lightState = "false";
-      if (lightSwitchState != LOW) {
-        lightState = "true";
-      }
-      response += "&lights=" + lightState;
-      String hazardsState = "false";
-      if (inHazardsMode) {
-        hazardsState = "true";
-      }
-      response += "&hazards=" + hazardsState + BLE_DELIMITER;
-      consolePrint(response);
-      break;
-    }
-    case COMMAND_DISCONNECT: {
-      bleConnected = false;
-      disableMotors();
-      break;
-    }
-    case COMMAND_SPEED_CHANGE: {
-      maxMotorSpeed = value.toInt();
-      EEPROM.put(0, byte(maxMotorSpeed));
-      break;
-    }
-    case COMMAND_FORWARD: {
-      currentHeading = command;
-      break;
-    }
-    case COMMAND_REVERSE: {
-      currentHeading = command;
-      break;
-    }
-    case COMMAND_LEFT: {
-      currentHeading = command;
-      break;
-    }
-    case COMMAND_RIGHT: {
-      currentHeading = command;
-      break;
-    }
-    case COMMAND_REVERSE_LEFT: {
-      currentHeading = command;
-      break;
-    }
-    case COMMAND_REVERSE_RIGHT: {
-      currentHeading = command;
-      break;
-    }
-    case COMMAND_LIGHTS_ON: {
-      toggleOnLights = true;
-      break;
-    }
-    case COMMAND_LIGHTS_OFF: {
-      toggleOffLights = true;
-      break;
-    }
-    case COMMAND_HAZARDS_ON: {
-      toggleOnHazards = true;
-      break;
-    }
-    case COMMAND_HAZARDS_OFF: {
-      toggleOffHazards = true;
-      break;
-    }
-    case COMMAND_STOP: {
-      currentHeading = command;
-      disableMotors();
-      break;
-    }
-  }
-}
-
-// Special formatting and a brief delay to keep debug
-// printing from mixing with the BLE print messages.
-void consolePrint(String message) {
-  Serial.println(message + BLE_DELIMITER);
-  delay(1);
-}
-
-void BLEListener() {
-  String inData = "";
-  while (HM10.available() > 0) {   // if HM10 sends something then read
-    if (!bleConnected) {
-      bleConnected = true;
-      disableMotors();
-    }
-    appData = HM10.read();
-    if (String(appData) != String(BLE_DELIMITER)) {
-      inData += String(appData);  // save the data in string format
-      delay(1);
-    } else {
-      if (inData.length()) {
-        processBLECommand(inData);
-        inData = "";
-      }
-    }
-  }
-}
+Cytron_SmartDriveDuo motor(PWM_INDEPENDENT, MOTOR_A_INPUT_1, MOTOR_A_INPUT_2, MOTOR_B_INPUT_1, MOTOR_B_INPUT_2);
 
 void setup()
 {
   Serial.begin(9600);
-  int tempSpeed;
-  EEPROM.get(0, tempSpeed);
-  if (tempSpeed != -1) {
-    maxMotorSpeed = int(tempSpeed);
-  }
-  Serial.println("max speed " + String(maxMotorSpeed));
-  
-  HM10.begin(9600);
-  
-  // set all the motor control pins to outputs
-  motorControllersInit();
 
-  // Set all the LEDs to output mode.
-  for (int i=0; i < LED_OUTPUTS_SIZE; i++) {
-    pinMode(LED_OUTPUTS[i], OUTPUT);
-  }
-  pinMode(HAZARDS_SWITCH, INPUT);
-  pinMode(LED_BUTTON_INPUT, INPUT);
-  pinMode(PILOT_MOTOR_REVERSE_SWITCH, INPUT);
-  digitalWrite(13, HIGH);
+  pinMode(GAS_PEDAL_INPUT, INPUT_PULLUP);
+  pinMode(HEADLIGHTS_SWITCH_INPUT, INPUT);
+  pinMode(DIRECTION_SWITCH_INPUT, INPUT_PULLUP);
+  pinMode(HAZARDS_SWITCH_INPUT, INPUT_PULLUP);
 
-  HM10.listen();
+  //toggles and buttons can be left on in various states so read in initial values
+  headlightsState = lastHeadlightsState = digitalRead(HEADLIGHTS_SWITCH_INPUT);
+  reverseState = lastReverseState = digitalRead(DIRECTION_SWITCH_INPUT);
+  hazardsState = lastHazardsState = digitalRead(HAZARDS_SWITCH_INPUT);
+  //gas should always initialize unpressed regardless of pedal state
+  gasState = lastGasState = LOW;
 }
-
-void motorControllersInit() {
-  disableMotors();
-}
-void disableMotors() {
-  consolePrint("turning off all motors");
-  bleMotor.control(0, 0);
-  pilotMotor.control(0, 0);
-}
-
-/*
-int accelerate(int target, int count) {
-  delay(10);
-  int divisor = (target/10)*count;
-  consolePrint("accelerating to " + String(divisor));
-  analogWrite(BLE_MOTOR_A_ENABLE, divisor);
-  if (divisor < target) {
-    count++;
-    return accelerate(target, count);
-  }
-  return 1;
-}
-*/
 
 void toggleAllLights(int dir) {
-    for (int i=0; i < LED_OUTPUTS_SIZE; i++) {
-      digitalWrite(LED_OUTPUTS[i], dir);
+  for (int i=0; i < LED_OUTPUTS_SIZE; i++) {
+    digitalWrite(LED_OUTPUTS[i], dir);
+  }
+}
+
+void runHazardsSequence() {
+  unsigned long currentMillis = millis();
+  // consolePrint(String(currentMillis) + " " + (previousMillis));
+  // consolePrint("DIFF : " + String(currentMillis - previousMillis));
+  int diff = currentMillis - previousHazardsMillis;
+  if (diff >= HAZARDS_INTERVAL ) {
+    if (!hazardsSequence) {
+      digitalWrite(LED_OUTPUTS[1], LOW);
+      digitalWrite(LED_OUTPUTS[0], HIGH);
+    } else {
+      digitalWrite(LED_OUTPUTS[0], LOW);
+      digitalWrite(LED_OUTPUTS[1], HIGH);
     }
+    hazardsSequence = !hazardsSequence;
+    previousHazardsMillis = currentMillis;
+  }
 }
 
 
 void loop()
 {
-  BLEListener();
-  //consolePrint("Switch: " + String(digitalRead(HAZARDS_SWITCH)));
+  //BLE STUFF HERE WHICH EXITS THE FUNCTION IF CONNECTED
 
-  if (bleConnected) {
-    if (currentHeading != previousHeading) {
-      switch(currentHeading) {
-        case COMMAND_FORWARD:
-          consolePrint("moving forward");
-          bleMotor.control(maxMotorSpeed, maxMotorSpeed);
-          break;
-        case COMMAND_LEFT:
-          bleMotor.control(maxMotorSpeed, 0);
-          break;
-        case COMMAND_RIGHT:
-          bleMotor.control(0, maxMotorSpeed);
-          break;
-        case COMMAND_REVERSE:
-          bleMotor.control(-maxMotorSpeed, -maxMotorSpeed);
-          break;
-        case COMMAND_REVERSE_LEFT:
-          bleMotor.control(-maxMotorSpeed, 0);
-          break;
-        case COMMAND_REVERSE_RIGHT:
-          bleMotor.control(0, -maxMotorSpeed);
-          break;
-        default:
-          disableMotors();
-      }
-      previousHeading = currentHeading;
-    }
-    if (toggleOnLights) {
-      toggleAllLights(true);
-      toggleOnLights = false;
-    }
-    if (toggleOffLights) {
-      toggleAllLights(false);
-      toggleOffLights = false;
-    }
-    if (toggleOnHazards) {
-       unsigned long currentMillis = millis();
-       // consolePrint(String(currentMillis) + " " + (previousMillis));
-       //consolePrint("DIFF : " + String(currentMillis - previousMillis));
-       int diff = currentMillis - previousMillis;
-       if (diff >= HAZARDS_INTERVAL ) {
-         if (!hazardState) {
-           digitalWrite(LED_OUTPUTS[1], LOW);
-           digitalWrite(LED_OUTPUTS[0], HIGH);
-         } else {
-           digitalWrite(LED_OUTPUTS[0], LOW);
-           digitalWrite(LED_OUTPUTS[1], HIGH);
-         }
-         hazardState = !hazardState;
-         previousMillis = currentMillis;
-      }
-    }
-    return;
+  int gasReading = digitalRead(GAS_PEDAL_INPUT);
+  int headlightsReading = digitalRead(HEADLIGHTS_SWITCH_INPUT);
+  int reverseReading = digitalRead(DIRECTION_SWITCH_INPUT);
+  int hazardsReading = digitalRead(HAZARDS_SWITCH_INPUT);
+
+  if (gasReading != lastGasState) {
+    lastGasDebounceTime = millis();
+  }
+  if (headlightsReading != lastHeadlightsState) {
+    lastHeadlightsDebounceTime = millis();
+  }
+  if (reverseReading != lastReverseState) {
+    lastReverseDebounceTime = millis();
+  }
+  if (hazardsReading != lastHazardsState) {
+    lastHazardsDebounceTime = millis();
   }
 
-  int pilotDirection = digitalRead(PILOT_MOTOR_REVERSE_SWITCH);
- // consolePrint("reverse status " + String(pilotDirection));
-  if (pilotDirection) {
-    pilotMotor.control(-maxMotorSpeed, -maxMotorSpeed);
-  } else {
-    pilotMotor.control(maxMotorSpeed, maxMotorSpeed);
-  }
-  
-  int actualLightSwitchState = digitalRead(LED_BUTTON_INPUT);
-  if (digitalRead(HAZARDS_SWITCH) == HIGH && !inHazardsMode) {
-   // consolePrint("Hazard lights active");
-    inHazardsMode = true;
-    previousMillis = millis();
-  } else if (digitalRead(HAZARDS_SWITCH) == LOW && inHazardsMode) {
-    //consolePrint("Hazard lights inactive");
-    inHazardsMode = false;
-    
-    lightSwitchState = actualLightSwitchState;
-    toggleAllLights(actualLightSwitchState);
-  }
-  if (actualLightSwitchState != lightSwitchState) {
-    lightSwitchState = actualLightSwitchState;
-    toggleAllLights(lightSwitchState);
-  }
-  
-  if (inHazardsMode) {
-    unsigned long currentMillis = millis();
-   // consolePrint(String(currentMillis) + " " + (previousMillis));
-   // consolePrint("DIFF : " + String(currentMillis - previousMillis));
-    int diff = currentMillis - previousMillis;
-    if (diff >= HAZARDS_INTERVAL ) {
-      if (!hazardState) {
-        digitalWrite(LED_OUTPUTS[1], LOW);
-        digitalWrite(LED_OUTPUTS[0], HIGH);
+  if ((millis() - lastGasDebounceTime) > debounceDelay) {
+    if (gasReading != gasState) {
+      gasState = gasReading;
+      if (gasState == HIGH) {
+        Serial.println("giving gas");
       } else {
-        digitalWrite(LED_OUTPUTS[0], LOW);
-        digitalWrite(LED_OUTPUTS[1], HIGH);
+        Serial.println("let off gas");
       }
-      hazardState = !hazardState;
-      previousMillis = currentMillis;
     }
+  }
+
+  if ((millis() - lastHeadlightsDebounceTime) > debounceDelay) {
+    if (headlightsReading != headlightsState) {
+      headlightsState = headlightsReading;
+      //toggleAllLights(headlightsState);
+      if (headlightsState == HIGH) {
+        Serial.println("lights on");
+      } else {
+        Serial.println("lights off");
+      }
+    }
+  }
+
+  if ((millis() - lastReverseDebounceTime) > debounceDelay) {
+    if (reverseReading != reverseState) {
+      reverseState = reverseReading;
+      if (gasState) {
+        if (reverseState == LOW) {
+          //moving forward
+          Serial.println("Direction forward");
+          motor.control(maxMotorSpeed, maxMotorSpeed);
+        } else {
+          Serial.println("direction reverse");
+          motor.control(-maxMotorSpeed, -maxMotorSpeed);
+        }
+      } else {
+        Serial.println("directional change but gas pedal off");
+      }
+    }
+  }
+
+  if ((millis() - lastHazardsDebounceTime) > debounceDelay) {
+    if (hazardsReading != hazardsState) {
+      hazardsState = hazardsReading;
+      if (hazardsState == LOW) {
+        Serial.println("haz on");
+        previousHazardsMillis = millis();
+      } else {
+        Serial.println("haz off");
+        toggleAllLights(headlightsState);
+      }
+    }
+  }
+
+  lastGasState = gasReading;
+  lastHeadlightsState = headlightsReading;
+  lastReverseState = reverseReading;
+  lastHazardsState = hazardsReading;
+
+  if (hazardsState) {
+    runHazardsSequence();
   }
 }
